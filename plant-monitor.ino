1816899ed244879d4246f16ba8d2c8a2386704ec
Jeff Anderson <jeff.anderson@digecor.com>
2017-01-21 16:18:21 -0700

    Add initial Arduino sketch

---
+++ plant-monitor.ino
@@ -0,0 +1,14 @@
+void setup() {
+	/*This is called when the program starts. It
+	basically sets up the board. Put any initialization
+	code here.*/
+}
+
+
+void loop() {
+	/*This is where your main code goes. This function
+	loops over and over, and allows you to respond to
+	input from your sensors. It only stops running when
+	the board is switched off*/
+}
+
